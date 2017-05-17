43383c22adaf23e94573e1c9cce41ebc2034add6
yueguoguo <yueguoguo1024@gmail.com>
2017-05-17 10:31:05 +0800

    Update demo cdoes

--- flightDelayPredictionWithDSVM/Code/sparkDemo/demo.R
+++ flightDelayPredictionWithDSVM/Code/sparkDemo/demo.R
@@ -1,5 +1,7 @@
 # ----------------------------------------------------------------------------
-# Data wrangling in Spark standalone mode.
+# Data pre-processing in Spark standalone mode.
+# 
+# This is a simplified version of codes from https://github.com/Azure/Azure-MachineLearning-DataScience/tree/master/Misc/StrataSanJose2017/Code/SparkR. 
 # ----------------------------------------------------------------------------
 
 # libraries to use.
@@ -8,8 +10,9 @@ library(SparkR)
 library(RevoScaleR)
 library(dplyr)
 library(magrittr)
-library(ggplot2)
 library(readr)
+library(AzureSMR)
+library(jsonlite)
 
 # data sources.
 
@@ -17,32 +20,31 @@ data_url <- "https://zhledata.blob.core.windows.net/mldata/air_1per.xdf"
 
 # data preparation.
 
-# data_dir <- file.path(getwd(), "demo/data")
 data_dir <- getwd()
-
-# system("hadoop fs -ls /home/zhle/demo/data") # necessary for standalone mode?
 
 data_xdf_path     <- file.path(data_dir, "air_data.xdf")
 data_csv_path     <- file.path(data_dir, "air_data.csv")
 data_parquet_path <- file.path(data_dir, "air_data_parquet")
 
-download.file(data_url, destfile=data_xdf_path)
+download.file(data_url, 
+              destfile=data_xdf_path, 
+              mode="wb")
 
-rxDataStep(inData=data_xdf_path, 
-           outFile=data_csv_path, 
-           overwrite=TRUE)
+RevoScaleR::rxDataStep(inData=data_xdf_path, 
+                       outFile=data_csv_path, 
+                       overwrite=TRUE)
 
 # initialize spark session.
 
-sc <- sparkR.session(
+sc <- SparkR::sparkR.session(
   sparkPackages="com.databricks:spark-csv_2.10:1.3.0"
 )
 
-setLogLevel("OFF")
+SparkR::setLogLevel("OFF")
 
 # data preparation - convert csv to parquet.
 
-sdf_air <- read.df(data_csv_path, 
+sdf_air <- SparkR::read.df(data_csv_path, 
                    source="com.databricks.spark.csv", 
                    header="true", 
                    inferSchema="true")
@@ -50,28 +52,20 @@ sdf_air <- read.df(data_csv_path,
 SparkR::cache(sdf_air) # cache into memory.
 SparkR::count(sdf_air) # count number of rows.
 
-write.df(df=sdf_air, 
+SparkR::write.df(df=sdf_air, 
          path=data_parquet_path, 
          "parquet", 
          "overwrite")
 
-sdf_air <- read.df(data_parquet_path, source="parquet") # write data into parquet format.
+sdf_air <- SparkR::read.df(data_parquet_path, source="parquet") # write data into parquet format.
 
-printSchema(sdf_air)
-
-SparkR::cache(sdf_air) 
-SparkR::count(sdf_air)
-
-head(sdf_air)
+SparkR::printSchema(sdf_air)
 
 # create a SQL context for manipulating the Spark data frames.
 
-createOrReplaceTempView(sdf_air, "air")
+SparkR::createOrReplaceTempView(sdf_air, "air")
 stable_air <-  SparkR::sql("SELECT a.DayofMOnth, a.DayOfWeek, a.Origin, a.Dest, a.DepTime, a.ArrDel15 FROM air a")  
-createOrReplaceTempView(stable_air, "stable_air")
-
-SparkR::cache(stable_air) 
-SparkR::count(stable_air)
+SparkR::createOrReplaceTempView(stable_air, "stable_air")
 
 head(SparkR::sql("show tables"))
 
@@ -86,32 +80,23 @@ sdf_air_sampled <- SparkR::sample(stable_air,
 
 df_air_sampled <- SparkR::as.data.frame(sdf_air_sampled)
 
-glimpse(df_air_sampled)
+dplyr::glimpse(df_air_sampled)
 
 df_air_sampled %<>% 
-  filter(as.character(ArrDel15) != "NA") %>%
-  mutate(DepTime=as.numeric(DepTime), ArrDel15=as.factor(ArrDel15)) %>%
-  na.omit()
+  dplyr::filter(as.character(ArrDel15) != "NA") %>%
+  dplyr::mutate(DepTime=as.numeric(DepTime), ArrDel15=as.factor(ArrDel15))
 
-ggplot(data=df_air_sampled, aes(x=DayOfWeek, y=DepTime)) +
-  geom_boxplot()
+# close Spark session.
 
-# ----------------------------------------------------------------------------
+SparkR::sparkR.session.stop()
+
 # Put the processed data onto storage account.
-# ----------------------------------------------------------------------------
 
-# load AzureSMR library.
-
-library(AzureSMR)
-library(jsonlite)
-
-context <- createAzureContext(tenantID=TID, clientID=CID, authKey=KEY)
+context <- AzureSMR::createAzureContext(tenantID=TID, clientID=CID, authKey=KEY)
 
 # convert data frame to json.
 
-js_air_sampled <- toJSON(df_air_sampled)
-
-write(js_air_sampled, file=file.path(data_dir, "processed_data.json"))
+js_air_sampled <- jsonlite::toJSON(df_air_sampled)
 
 # create a blob to preserve data.
 
