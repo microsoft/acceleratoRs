b33e335861fd33beea5088741a59fc4a0d45a6f0
Zhang Le <yueguoguo@users.noreply.github.com>
2017-08-18 17:25:37 +0800

    Specify installation lib path

--- SolarPanelForecasting/Code/script.sh
+++ SolarPanelForecasting/Code/script.sh
@@ -2,7 +2,7 @@
 
 # install R libraries.
 
-sudo Rscript -e 'library(devtools);install_github(c("rstudio/reticulate", "gaborcsardi/debugme", "r-lib/processx", "rstudio/tfruns", "rstudio/keras"))'
+sudo Rscript -e 'library(devtools);with_libpaths(new="~/R/lib", install_github(c("rstudio/reticulate", "gaborcsardi/debugme", "r-lib/processx", "rstudio/tfruns", "rstudio/keras")))'
 
 # Create keras config json file.
 
